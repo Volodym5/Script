3bcee8945c57dbfc63202f45a06930a8b9839f5c
Volodym5 <116100568+Volodym5@users.noreply.github.com>
2024-06-22 13:31:46 +0300

    Update CounterBlox.lua

--- CounterBlox/CounterBlox.lua
+++ CounterBlox/CounterBlox.lua
@@ -1,3 +1,5 @@
 loadstring(game:HttpGet("https://gist.githubusercontent.com/Volodym5/1db7c6fb157db457b3444f605368b8d8/raw/95c3e706f6609a036b41bd80db07d460e1ef195e/CounterBloxScript.txt"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ttwizz/Open-Aimbot/master/source.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Qrto1/working/main/pc"))()
