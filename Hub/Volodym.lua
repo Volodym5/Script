d62e3a17741d33e8ce53987bb15c5843f2e23102
Volodym5 <116100568+Volodym5@users.noreply.github.com>
2024-07-20 11:55:10 +0300

    Update Volodym.lua

--- Hub/Volodym.lua
+++ Hub/Volodym.lua
@@ -242,6 +242,14 @@ do
         end
     })
 
+        Tabs.CounterBlox:AddButton({
+        Title = "Happy Hub",
+        Description = "",
+        Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/uedan228/FREE-Counter-Blox-script/main/V1.1"))()
+        end
+    })
+
     Tabs.Evade:AddButton({
         Title = "Tbao",
         Description = "",
