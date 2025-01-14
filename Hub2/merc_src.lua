576ffe083b5e5006c9ad2bfdcf5e07c70e7e2709
Volodym5 <116100568+Volodym5@users.noreply.github.com>
2025-01-14 20:49:40 +0200

    Update merc_src.lua

--- Hub2/merc_src.lua
+++ Hub2/merc_src.lua
@@ -48,6 +48,15 @@ local Library = {
 			StrongText = Color3.fromHSV(0, 0, 1),        
 			WeakText = Color3.fromHSV(0, 0, 172/255)
 		},
+		Darker = {
+			Main = Color3.fromRGB(15, 15, 20),
+			Secondary = Color3.fromRGB(30, 30, 35), --
+			Tertiary = Color3.fromRGB(40, 80, 110),
+
+			StrongText = Color3.fromHSV(0, 0, 0.85),
+			WeakText = Color3.fromHSV(0, 0, 0.5)
+
+		},
 		Vaporwave = {},
 		OperaGX = {},
 		VisualStudio = {}
