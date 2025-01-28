01f2473b2694758fd0514b1202f5d0412a0db171
Volodym5 <116100568+Volodym5@users.noreply.github.com>
2025-01-28 19:58:36 +0200

    Update Volodym.lua

--- Hub/Volodym.lua
+++ Hub/Volodym.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Script Hub",
-    SubTitle = "by Volodym5",
+    Title = "Uranium Hub",
+    SubTitle = "",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
